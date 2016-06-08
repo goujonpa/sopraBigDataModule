c484e753835b5de9b108de3f00e489a7562d7b81
goujonpa <goujonpa@gmail.com>
2016-06-08 18:17:16 +0200

    Init the project
    1. Django Setup
    2. Postgresql Setup
    3. Some Django settings
    4. API module
    5. First API view

---
+++ dev.sh
@@ -0,0 +1,6 @@
+# source venv
+source venv/bin/activate
+
+# database start command
+alias database_start='pg_ctl -D /usr/local/var/postgresql/data/ -l
+/usr/local/var/postgresql/psql.log start'
---
+++ sopraBDM/API/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import View 
+from django.http import JsonResponse
+
+# Create your views here.
+
+class HWView(View):
+    """Hello World API View"""
+
+    def get(self, request):
+        """Returns a JSON Response saying "Hello World"""
+        return JsonResponse({"response": "Hellow World !"});
+
